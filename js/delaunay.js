5ed34d8017e0b4b528a3c8754e90fc8deb7d958b
Reivajar <reivajar@gmail.com>
2015-01-04 03:23:04 +0100

    added

---
+++ js/delaunay.js
@@ -0,0 +1,156 @@
+// JavaScript Document
+$(document).ready(function () {
+
+       	$("#escalera_recta_grande_CURVAS").hover(function (){
+            var idElement = this.id;
+            $("#escalera_recta_grande_LINKS").css('opacity', '1');
+       });
+	   
+	   	$("#escalera_recta_grande_CURVAS").mouseleave(function () {
+            var idElement = this.id;
+            $("#escalera_recta_grande_LINKS").css('opacity', '0');
+        });
+		
+
+		$("#escalera_recta_peq_CURVAS").hover(function (){
+       		var idElement = this.id;
+         $("#escalera_recta_peq_LINKS").css('opacity', '1');
+       	});
+	   
+	   	$("#escalera_recta_peq_CURVAS").mouseleave(function (){
+            var idElement = this.id;
+            $("#escalera_recta_peq_LINKS").css('opacity', '0');
+        });
+		
+		
+		$("#escalera_redonda_grande_CURVAS").hover(function (){
+            var idElement = this.id;
+            $("#escalera_redonda_grande_LINKS").css('opacity', '1');
+        });
+		
+		$("#escalera_redonda_grande_CURVAS").mouseleave(function (){
+            var idElement = this.id;
+            $("#escalera_redonda_grande_LINKS").css('opacity', '0');
+        });
+		
+		
+		$("#escalera_redonda_peq_CURVAS").hover(function (){
+            var idElement = this.id;
+            $("#escalera_redonda_peq_LINKS").css('opacity', '1');
+        });
+		
+		$("#escalera_redonda_peq_CURVAS").mouseleave(function (){
+            var idElement = this.id;
+            $("#escalera_redonda_peq_LINKS").css('opacity', '0');
+        });
+		
+		
+		$("#montacargas_CURVAS").hover(function (){
+            var idElement = this.id;
+            $("#montacargas_LINKS").css('opacity', '1');
+        });
+		
+		$("#montacargas_CURVAS").mouseleave(function (){
+            var idElement = this.id;
+            $("#montacargas_LINKS").css('opacity', '0');
+        });
+		
+		
+		$("#ascensor_xUNO_CURVAS").hover(function (){
+            var idElement = this.id;
+            $("#ascensor_xUNO_LINKS").css('opacity', '1');
+        });
+		
+		$("#ascensor_xUNO_CURVAS").mouseleave(function (){
+            var idElement = this.id;
+            $("#ascensor_xUNO_LINKS").css('opacity', '0');
+        });
+		
+		
+		$("#ascensor_xDOS_CURVAS").hover(function (){
+            var idElement = this.id;
+            $("#ascensor_xDOS_LINKS").css('opacity', '1');
+        });
+		
+		$("#ascensor_xDOS_CURVAS").mouseleave(function (){
+            var idElement = this.id;
+            $("#ascensor_xDOS_LINKS").css('opacity', '0');
+        });
+		
+		
+		$("#ascensor_xTRES_CURVAS").hover(function (){
+            var idElement = this.id;
+            $("#ascensor_xTRES_LINKS").css('opacity', '1');
+        });
+		
+		$("#ascensor_xTRES_CURVAS").mouseleave(function (){
+            var idElement = this.id;
+            $("#ascensor_xTRES_LINKS").css('opacity', '0');
+        });
+		
+		
+		$("#Misc_peq_CURVAS").hover(function (){
+            var idElement = this.id;
+            $("#Misc_peq_LINKS").css('opacity', '1');
+        });
+		
+		$("#Misc_peq_CURVAS").mouseleave(function (){
+            var idElement = this.id;
+            $("#Misc_peq_LINKS").css('opacity', '0');
+        });
+		
+		
+		$("#Misc_med_CURVAS").hover(function (){
+            var idElement = this.id;
+            $("#Misc_med_LINKS").css('opacity', '1');
+        });
+		
+		$("#Misc_med_CURVAS").mouseleave(function (){
+            var idElement = this.id;
+            $("#Misc_med_LINKS").css('opacity', '0');
+        });
+		
+		
+		$("#Misc_grande_CURVAS").hover(function (){
+            var idElement = this.id;
+            $("#Misc_grande_LINKS").css('opacity', '1');
+        });
+		
+		$("#Misc_grande_CURVAS").mouseleave(function (){
+            var idElement = this.id;
+            $("#Misc_grande_LINKS").css('opacity', '0');
+        });
+		
+		
+		$("#Deportivo_peq_CURVAS").hover(function (){
+            var idElement = this.id;
+            $("#Deportivo_peq_LINKS").css('opacity', '1');
+        });
+		
+		$("#Deportivo_peq_CURVAS").mouseleave(function (){
+            var idElement = this.id;
+            $("#Deportivo_peq_LINKS").css('opacity', '0');
+        });
+		
+		
+		$("#Deportivo_med_CURVAS").hover(function (){
+            var idElement = this.id;
+            $("#Deportivo_med_LINKS").css('opacity', '1');
+        });
+		
+		$("#Deportivo_med_CURVAS").mouseleave(function (){
+            var idElement = this.id;
+            $("#Deportivo_med_LINKS").css('opacity', '0');
+        });
+		
+		
+		$("#Deportivo_grande_CURVAS").hover(function (){
+            var idElement = this.id;
+            $("#Deportivo_grande_LINKS").css('opacity', '1');
+        });
+		
+		$("#Deportivo_grande_CURVAS").mouseleave(function (){
+            var idElement = this.id;
+            $("#Deportivo_grande_LINKS").css('opacity', '0');
+        });
+}); 
